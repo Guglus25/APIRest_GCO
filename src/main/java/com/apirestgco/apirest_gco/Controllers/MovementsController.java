98d03ecfa1a6201c54ffbf1f27fd4db2d436a68c
Cristian Felipe Jaramillo Medina <crisfel25@hotmail.com>
2025-05-25 20:36:28 -0500

    documentacion de los endpoint

--- src/main/java/com/apirestgco/apirest_gco/Controllers/MovementsController.java
+++ src/main/java/com/apirestgco/apirest_gco/Controllers/MovementsController.java
@@ -9,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apirestgco.apirest_gco.Models.MovementsModel;
+import com.apirestgco.apirest_gco.Models.ProductsModel;
 import com.apirestgco.apirest_gco.Services.MovementsService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,11 +31,17 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RestController
 @RequestMapping("/movement")
 @CrossOrigin(origins = "http://localhost:4200")
-@Tag(name = "Movement Management", description = "Controllers Movements")
+@Tag(name = "Gestión de movimientos", description = "Controlador de Movimientos")
 public class MovementsController {
     @Autowired
     MovementsService movementService;
 
+    @Operation(summary = "Consultar los movimientos del producto por id", description = "Este endpoint devuelve los registro de movimiento de un producto en especifico por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Movimiento consultado", content = @Content(schema = @Schema(implementation = ProductsModel.class))),
+            @ApiResponse(responseCode = "400", description = "Datos de solicitud no válidos", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @GetMapping(value = "/product/{idproducto}")
     public ResponseEntity<?> getfindMovements(@PathVariable("idproducto")Long idproducto) {
         try {
@@ -40,6 +53,13 @@ public class MovementsController {
         }
     }
 
+
+    @Operation(summary = "Consultar los movimientos por id", description = "Este endpoint devuelve el registro de movimiento en especifico por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Movimiento consultado", content = @Content(schema = @Schema(implementation = ProductsModel.class))),
+            @ApiResponse(responseCode = "400", description = "Datos de solicitud no válidos", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @GetMapping(value = "/{id}")
     public ResponseEntity<?> getfindMovementsByid(@PathVariable("id") Long id) {
         try {
@@ -57,6 +77,20 @@ public class MovementsController {
     }
 
     
+    @Operation(summary = "Creación del movimiento", description = "Este endpoint crea el movimiento de un producto en la base de datos, recibe el modelo de datos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Movimiento creado", content = @Content(schema = @Schema(implementation = MovementsModel.class), examples = @ExampleObject(name = "Respuesta exitosa", value = """
+                    {
+                        "idProducto":1,
+                        "tipo":"Entrada",
+                        "cantidad":4,
+                        "fecha":"2025-05-18",
+                        "descripcion":"nuevo Producto"
+                    }
+                    """))),
+            @ApiResponse(responseCode = "400", description = "Datos de solicitud no válidos", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @PostMapping
     public ResponseEntity<?> postMovements(@RequestBody MovementsModel entity) {
         try {
@@ -69,6 +103,20 @@ public class MovementsController {
 
     }
 
+     @Operation(summary = "Actualizar el movimiento", description = "Este endpoint actualiza un movimiento por id y recibe el modelo de datos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "movimiento actualizado", content = @Content(schema = @Schema(implementation = MovementsModel.class), examples = @ExampleObject(name = "Respuesta exitosa", value = """
+                    {
+                        "idProducto":1,
+                        "tipo":"Entrada",
+                        "cantidad":4,
+                        "fecha":"2025-05-18",
+                        "descripcion":"nuevo Producto"
+                    }
+                    """))),
+            @ApiResponse(responseCode = "400", description = "Datos de solicitud no válidos", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @PutMapping(value = "/{id}")
     public ResponseEntity<?> putMovements(@PathVariable("id") Long id, @RequestBody MovementsModel entity) {
         try {
@@ -81,6 +129,12 @@ public class MovementsController {
         }
     }
 
+    @Operation(summary = "Eliminar movimiento por id", description = "Este endpoint elimina el registro de un movimiento en especifico por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Movimiento eliminado", content = @Content(examples = @ExampleObject(name = "Fue eliminado"))),
+            @ApiResponse(responseCode = "400", description = "Datos de solicitud no válidos", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> deleteMovements(@PathVariable("id") Long id) {
         try {
--- src/main/java/com/apirestgco/apirest_gco/Controllers/ProductsController.java
+++ src/main/java/com/apirestgco/apirest_gco/Controllers/ProductsController.java
@@ -14,6 +14,11 @@ import com.apirestgco.apirest_gco.Models.ProductsModel;
 import com.apirestgco.apirest_gco.Services.ProductsService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,19 +26,21 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @RestController
 @RequestMapping("/product")
 @CrossOrigin(origins = "http://localhost:4200")
-@Tag(name = "Product Management", description = "Controllers Product")
+@Tag(name = "Gestión de productos", description = "Controlador de Productos")
 public class ProductsController {
     @Autowired
     ProductsService productsService;
 
-    
+    @Operation(summary = "Consultar todos los productos", description = "Este endpoint devuelve todos los registros de los productos creados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Productos consultados", content = @Content(schema = @Schema(implementation = ProductsModel.class))),
+            @ApiResponse(responseCode = "400", description = "Datos de solicitud no válidos", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @GetMapping
-    @Operation(summary = "", description = "Endpoint que trae los datos de la solicitud", tags = {
-            "Product Management" })    
     public ResponseEntity<?> getfindProduct() {
         try {
             List<ProductsModel> Products = productsService.findAllProduct();
@@ -45,6 +52,12 @@ public class ProductsController {
 
     }
 
+    @Operation(summary = "Consultar los productos por id", description = "Este endpoint devuelve el registro especifico por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Producto consultado", content = @Content(schema = @Schema(implementation = ProductsModel.class))),
+            @ApiResponse(responseCode = "400", description = "Datos de solicitud no válidos", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @GetMapping(value = "/{id}")
     public ResponseEntity<?> findProductid(@PathVariable("id") Long id) {
         try {
@@ -60,6 +73,22 @@ public class ProductsController {
         }
     }
 
+    @Operation(summary = "Creación de producto", description = "Este endpoint crea el producto en la base de datos, recibe el modelo de datos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Producto creado", content = @Content(schema = @Schema(implementation = ProductsModel.class), examples = @ExampleObject(name = "Respuesta exitosa", value = """
+                    {
+                        "nombre":"Producto ",
+                        "descripcion":"Prueba para almacenar",
+                        "precio":20000,
+                        "stock":"6",
+                        "categoria":"Pan",
+                        "codigo":"C001",
+                        "fechaCreacion":"2025-05-18"
+                    }
+                    """))),
+            @ApiResponse(responseCode = "400", description = "Datos de solicitud no válidos", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @PostMapping
     public ResponseEntity<?> postProducts(@RequestBody ProductsModel entity) {
         try {
@@ -72,6 +101,22 @@ public class ProductsController {
 
     }
 
+    @Operation(summary = "Actualizar el producto", description = "Este endpoint actualiza un producto por id y recibe el modelo de datos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Producto actualizado", content = @Content(schema = @Schema(implementation = ProductsModel.class), examples = @ExampleObject(name = "Respuesta exitosa", value = """
+                    {
+                        "nombre":"Producto ",
+                        "descripcion":"Prueba para almacenar",
+                        "precio":20000,
+                        "stock":"6",
+                        "categoria":"Pan",
+                        "codigo":"C001",
+                        "fechaCreacion":"2025-05-18"
+                    }
+                    """))),
+            @ApiResponse(responseCode = "400", description = "Datos de solicitud no válidos", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @PutMapping(value = "/{id}")
     public ResponseEntity<?> updateProduct(@PathVariable long id, @RequestBody ProductsModel Product) {
         try {
@@ -83,11 +128,17 @@ public class ProductsController {
         }
     }
 
+    @Operation(summary = "Eliminar producto por id", description = "Este endpoint elimina el registro de un producto en especifico por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Producto eliminado", content = @Content(examples = @ExampleObject(name = "Eliminado"))),
+            @ApiResponse(responseCode = "400", description = "Datos de solicitud no válidos", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> deleteProduct(@PathVariable("id") Long id) {
         try {
             productsService.deleteProducts(id);
-            return new ResponseEntity<String>( "Eliminado", HttpStatus.OK);
+            return new ResponseEntity<String>("Eliminado", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<String>(e.getCause().toString(),
                     HttpStatus.INTERNAL_SERVER_ERROR);
